5d8ecbd5bf10e87456e772f62f06536e4bbee241
Hugo Fraga <hugohgfraga1@gmail.com>
2024-10-29 17:24:59 -0300

    first commit

--- src/App.java
+++ src/App.java
@@ -3,10 +3,14 @@ public class App {
         Fila fila = new Fila();
         fila.enfileirar("A");
         fila.enfileirar("B");
+        fila.enfileirar("A");
         fila.enfileirar("C");
 
         System.out.println("Fila original: " + fila);
         fila.inverter();
-        System.out.println("Fila invertida: " + fila); // Deve imprimir "[C, B, A]"
+        System.out.println("Fila invertida: " + fila); 
+
+        int contagem = fila.contarOcorrencias("A");
+        System.out.println("Contagem de 'A': " + contagem);
     }
 }
